ad6cf79a666f3546dcabbb2e581ac4008f429dfe
Fournet Enzo <enzofournet31@gmail.com>
2023-05-23 15:58:47 +0200

    FIX EXE Batch

--- V2/Dev/Code/DailyBank-Batch/src/main/java/BatchApp.java
+++
@@ -1,8 +0,0 @@
-import application.Batch;
-
-public class BatchApp {
-    public static void main( String[] args ) {
-		Batch batch = new Batch();
-        batch.start();
-    }
-}
---
+++ V2/Dev/Code/DailyBank-Batch/src/main/java/app/BatchApp.java
@@ -0,0 +1,10 @@
+package app;
+
+import app.control.Batch;
+
+public class BatchApp {
+    public static void main( String[] args ) {
+		Batch batch = new Batch();
+        batch.start();
+    }
+}
